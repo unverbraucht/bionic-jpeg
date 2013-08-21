9059afc1413e31066c879026ba72fcd61a27fc58
John McAleely <john.mcaleely@canonical.com>
2013-08-21 15:00:22 +0100

    build a functioning libjpeg and cjpeg combo for android

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -2,7 +2,30 @@ LOCAL_PATH := ../jpeg-9/
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := ckconfig
-LOCAL_SRC_FILES := ckconfig.c
+LOCAL_MODULE := libjpeg
+LOCAL_C_INCLUDES := jni
+LOCAL_SRC_FILES := jaricom.c jcapimin.c jcapistd.c jcarith.c jccoefct.c jccolor.c \
+        jcdctmgr.c jchuff.c jcinit.c jcmainct.c jcmarker.c jcmaster.c \
+        jcomapi.c jcparam.c jcprepct.c jcsample.c jctrans.c jdapimin.c \
+        jdapistd.c jdarith.c jdatadst.c jdatasrc.c jdcoefct.c jdcolor.c \
+        jddctmgr.c jdhuff.c jdinput.c jdmainct.c jdmarker.c jdmaster.c \
+        jdmerge.c jdpostct.c jdsample.c jdtrans.c jerror.c jfdctflt.c \
+        jfdctfst.c jfdctint.c jidctflt.c jidctfst.c jidctint.c jquant1.c \
+        jquant2.c jutils.c jmemmgr.c jmemnobs.c
+
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := cjpeg
+LOCAL_C_INCLUDES := jni
+LOCAL_SRC_FILES := cjpeg.c \
+	cdjpeg.c \
+        rdcolmap.c rdswitch.c transupp.c rdppm.c wrppm.c rdgif.c wrgif.c \
+        rdtarga.c wrtarga.c rdbmp.c wrbmp.c rdrle.c wrrle.c
+
+LOCAL_SHARED_LIBRARIES := libjpeg
 
 include $(BUILD_EXECUTABLE)
