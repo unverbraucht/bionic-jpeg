7fdf4b5ccff23ae077d6ecaed64f8777ab6bd7da
John McAleely <john.mcaleely@canonical.com>
2013-08-27 14:44:40 +0100

    now compiles clients against hybris-bridge

--- hybris-bridge/libjpeg-bridge.c
+++ hybris-bridge/libjpeg-bridge.c
@@ -44,3 +44,19 @@ void jpeg_add_quant_table(j_compress_ptr param1, int param2, const unsigned int 
 HYBRIS_IMPLEMENT_FUNCTION1(libjpeg, int, jpeg_quality_scaling, int);
 HYBRIS_IMPLEMENT_VOID_FUNCTION2(libjpeg, jpeg_default_qtables, j_compress_ptr, boolean);
 
+HYBRIS_IMPLEMENT_VOID_FUNCTION3(libjpeg, jpeg_set_marker_processor, j_decompress_ptr, int, jpeg_marker_parser_method);
+HYBRIS_IMPLEMENT_VOID_FUNCTION2(libjpeg, jpeg_stdio_src, j_decompress_ptr, FILE*);
+HYBRIS_IMPLEMENT_FUNCTION2(libjpeg, int, jpeg_read_header, j_decompress_ptr, boolean);
+HYBRIS_IMPLEMENT_FUNCTION1(libjpeg, boolean, jpeg_start_decompress, j_decompress_ptr);
+HYBRIS_IMPLEMENT_FUNCTION3(libjpeg, JDIMENSION, jpeg_read_scanlines, j_decompress_ptr, JSAMPARRAY, JDIMENSION);
+HYBRIS_IMPLEMENT_VOID_FUNCTION1(libjpeg, jpeg_calc_output_dimensions, j_decompress_ptr);
+
+HYBRIS_IMPLEMENT_FUNCTION1(libjpeg, jvirt_barray_ptr*, jpeg_read_coefficients, j_decompress_ptr);
+HYBRIS_IMPLEMENT_VOID_FUNCTION2(libjpeg, jpeg_copy_critical_parameters, j_decompress_ptr, j_compress_ptr);
+HYBRIS_IMPLEMENT_VOID_FUNCTION2(libjpeg, jpeg_write_coefficients, j_compress_ptr, jvirt_barray_ptr *);
+HYBRIS_IMPLEMENT_VOID_FUNCTION1(libjpeg, jpeg_core_output_dimensions, j_decompress_ptr);
+HYBRIS_IMPLEMENT_VOID_FUNCTION3(libjpeg, jpeg_save_markers, j_decompress_ptr, int, unsigned int);
+HYBRIS_IMPLEMENT_VOID_FUNCTION4(libjpeg, jpeg_write_marker, j_compress_ptr, int, const JOCTET *, unsigned int);
+
+HYBRIS_IMPLEMENT_VOID_FUNCTION3(libjpeg, jcopy_block_row, JBLOCKROW, JBLOCKROW, JDIMENSION);
+HYBRIS_IMPLEMENT_FUNCTION2(libjpeg, long, jdiv_round_up, long, long);
